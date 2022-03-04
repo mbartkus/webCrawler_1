837fb5512821730c2063c41755327f5a14598d55
mbartkus <mbartkus.it@gmail.com>
2022-03-04 03:38:44 +0200

    2

--- Program.cs
+++ Program.cs
@@ -1,7 +1,10 @@
 ﻿using System;
+using System.Linq;
 using System.Net.Http;
 using HtmlAgilityPack;
-using 
+using System.Threading.Tasks;
+//using ;
+
 namespace webCrawler_1
 {
     class Program
@@ -9,30 +12,49 @@ namespace webCrawler_1
         static void Main(string[] args)
         {
             //Varibales:
-            string url;
+            AsyncStartCrawl();
 
-       
-;
+            Console.WriteLine("test");
+            Console.ReadLine();
 
 
-            }
+        }
 
-        private static async Task StartCrawler(string url, HtmlDocument htmlDoc)
+
+
+
+
+
+        private static async Task AsyncStartCrawl()
         {
+            var xpath = "/html/body/div[1]/div/section/div[3]/form/div[1]/div[2]/div[1]/table/tbody/tr/td[4]/span[2]";
+            string url = "https://www.fly540.com/flights/nairobi-to-mombasa?isoneway=1&depairportcode=NBO&arrvairportcode=MBA&date_from=Mon%2C+2+May+2022&date_to=Wed%2C+4+May+2022&adult_no=1&children_no=0&infant_no=0&currency=USD&searchFlight=";
+            //string url = "https://www.fly540.com/flights";
 
 
             //seting up HttpClient
             var http_client = new HttpClient();
             //GET request
-            http_client.GetStringAsync(url);
-
+            var httpGet = await http_client.GetStringAsync(url);
 
             //settingup doc
-            var htmlDoc = new HtmlDocument();
-            htmlDoc.LoadHtml(url);
-            htmlDoc.DocumentNode.Descendants("<div>").Where()node => node.get)
+            var html_Doc = new HtmlDocument();
+            html_Doc.LoadHtml(httpGet);
+
+            //AllTrips
+            var listed_trips = html_Doc.DocumentNode.Descendants("div").Where(node => node.GetAttributeValue("class", "").Equals("fly5-results")).ToList();
+            // <span class="flprice"
+            var single_trip = listed_trips[0].Descendants("span").Where(node => node.GetAttributeValue("class", "").Equals("flprice")).ToList();
+            foreach (var obj_Div in listed_trips)
+            {
+                var listed_Divs = html_Doc.DocumentNode.SelectNodes(xpath);
+            }
+        //   foreach (var obj_Div in listed_Divs)
+          //  {
+             //  obj_Div.Descendants("tr-link").
+
+            }
+
         }
 
-
-     }
-
+    }
